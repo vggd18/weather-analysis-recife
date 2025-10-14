91a5b9f9c290c6996183b12b997372b4be15ec5f
Vitor Gabriel Gomes Dias <seu_email_github@exemplo.com>
2025-10-14 00:24:57 -0300

    feat(deploy): finalize terraform config and define build process

--- src/lambda_ingestion/index.py
+++ src/lambda_ingestion/index.py
@@ -5,32 +5,38 @@ import boto3
 import json
 from datetime import datetime
 
-##### ENVIRONMENT VARIABLES #####
-load_dotenv()
-s3_bucket_name = os.getenv("S3-BUCKET-NAME")
-weather_api_key = os.getenv("WEATHER-API-KEY") 
+def lambda_handler(event, context):
+  ##### ENVIRONMENT VARIABLES #####
+  load_dotenv()
+  s3_bucket_name = os.getenv("S3-BUCKET-NAME")
+  weather_api_key = os.getenv("WEATHER-API-KEY") 
 
-##### WEATHER API REQUEST #####
-city = "Recife,BR"
-parameters = {
-  "q": city,
-  "appid": weather_api_key,
-  "units": "metric"
-}
-response = requests.get(
-  f"https://api.openweathermap.org/data/2.5/weather",
-  params=parameters
-)
+  ##### WEATHER API REQUEST #####
+  city = "Recife,BR"
+  parameters = {
+    "q": city,
+    "appid": weather_api_key,
+    "units": "metric"
+  }
+  response = requests.get(
+    f"https://api.openweathermap.org/data/2.5/weather",
+    params=parameters
+  )
 
-data = response.json()
-json_data = json.dumps(data, indent=2)
+  data = response.json()
+  json_data = json.dumps(data, indent=2)
 
-now = datetime.now()
-file_key = now.strftime("%Y/%m/%d/%H-%M-%S.json") 
+  now = datetime.now()
+  file_key = now.strftime("%Y/%m/%d/%H-%M-%S.json") 
 
-s3 = boto3.client('s3')
-s3.put_object(
-    Bucket=s3_bucket_name,
-    Key=file_key,
-    Body=json_data
-)
+  s3 = boto3.client('s3')
+  s3.put_object(
+      Bucket=s3_bucket_name,
+      Key=file_key,
+      Body=json_data
+  )
+
+  return {
+      'statusCode': 200,
+      'body': json.dumps(f'File {file_key} saved to {s3_bucket_name}!')
+  }
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,3 +11,63 @@ resource "aws_s3_bucket" "data_lake" {
     owner   = "DataEngineeringTeam"
   }
 }
+
+resource "aws_iam_role" "lambda_execution_role" {
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_policy" {
+  name = "weather_lambda_policy"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement" = [
+      {
+        "Effect": "Allow",
+        "Action": [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        "Resource": "arn:aws:logs:*:*:*"
+      },
+      {      
+        "Effect": "Allow",
+        "Action": ["s3:PutObject"],
+        "Resource": ["${aws_s3_bucket.data_lake.arn}/*"]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_execution_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+data "archive_file" "lambda_zip" {
+  type = "zip"
+  source_dir  = "../build/lambda_package"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "weather_ingestion_lambda" {
+  function_name     = "weather_ingestion_recife"
+  role              = aws_iam_role.lambda_execution_role.arn
+  handler           = "index.lambda_handler"
+  runtime           = "python3.9"
+  filename          = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+}
+
